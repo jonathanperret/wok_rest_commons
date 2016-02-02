02e72ca3317faaae2f7d2b208a2313774a85fc0f
sulphur <piotr.kedziora27@gmail.com>
2016-02-02 14:21:42 +0100

    remove warrning

--- spec/wok_rest_commons/json_api_response_renderer_spec.exs
+++ spec/wok_rest_commons/json_api_response_renderer_spec.exs
@@ -1,12 +1,11 @@
 defmodule JSONAPIResponseRendererSpec do
-  alias WokRESTCommons.JSONAPIResponseRenderer
   use ESpec
 
   describe "respond_with" do
     let :response, do: %WokRESTCommons.Response{body: "{\"id\": 1}" ,status_code: 200}
     let :default_json_api_headers, do: [{"content-type", "application/vnd.api+json"}, {"Access-Control-Allow-Origin", "*"}]
 
-    subject do: JSONAPIResponseRenderer.respond_with(response, %{})
+    subject do: WokRESTCommons.JSONAPIResponseRenderer.respond_with(response, %{})
 
     it do: is_expected.to eq({200, "{\"id\": 1}", default_json_api_headers, %{}})
 

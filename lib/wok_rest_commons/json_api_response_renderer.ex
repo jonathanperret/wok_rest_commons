41a0c5e906bd36dbda8e657f8cf9f928dffc78e9
sulphur <piotr.kedziora27@gmail.com>
2016-02-02 14:01:58 +0100

    rename method

--- lib/wok_rest_commons/json_api_response_renderer.ex
+++ lib/wok_rest_commons/json_api_response_renderer.ex
@@ -1,5 +1,5 @@
 defmodule WokRESTCommons.JSONAPIResponseRenderer do
-  def render_response(response, state) do
+  def respond_with(response, state) do
     {response.status_code, response.body, response.headers, state}
   end
 end
--- spec/wok_rest_commons/json_api_response_renderer_spec.exs
+++ spec/wok_rest_commons/json_api_response_renderer_spec.exs
@@ -2,11 +2,11 @@ defmodule JSONAPIResponseRendererSpec do
   alias WokRESTCommons.JSONAPIResponseRenderer
   use ESpec
 
-  describe "render_response" do
+  describe "respond_with" do
     let :response, do: %WokRESTCommons.Response{body: "{\"id\": 1}" ,status_code: 200}
     let :default_json_api_headers, do: [{"content-type", "application/vnd.api+json"}, {"Access-Control-Allow-Origin", "*"}]
 
-    subject do: JSONAPIResponseRenderer.render_response(response, %{})
+    subject do: JSONAPIResponseRenderer.respond_with(response, %{})
 
     it do: is_expected.to eq({200, "{\"id\": 1}", default_json_api_headers, %{}})
 

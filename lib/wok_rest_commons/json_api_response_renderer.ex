b157ef3c96e5265d61f96edc39b927771514ae04
sulphur <piotr.kedziora27@gmail.com>
2016-02-05 00:24:58 +0100

    fix error

--- lib/wok_rest_commons/json_api_response_renderer.ex
+++ lib/wok_rest_commons/json_api_response_renderer.ex
@@ -1,6 +1,6 @@
 defmodule WokRESTCommons.JSONAPIResponseRenderer do
   def respond_with(response), do: raise "2nd param(state) required "
   def respond_with(response, state) do
-    {response.status_code, response.body, response.headers, state}
+    {response.status_code, response.headers, response.body, state}
   end
 end

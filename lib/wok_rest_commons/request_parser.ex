8793ef1b10b41095d4078dd21d12be2d18433aa6
sulphur <piotr.kedziora27@gmail.com>
2016-02-05 11:46:03 +0100

    add conversion from dasherized-param-names to snake_case

--- lib/wok_rest_commons/request_parser.ex
+++ lib/wok_rest_commons/request_parser.ex
@@ -2,11 +2,24 @@ defmodule WokRESTCommons.RequestParser do
   def parse_request(response, req) do
     decoded = req |> :cowboy_req.body |> decode_body_from_response
     case decoded do
-      {:ok, decoded_body} -> {:ok, %{response | decoded_body: decoded_body}}
+      {:ok, decoded_body} -> {:ok, %{response | decoded_body: convert_dashes(decoded_body)}}
       {:error, _} -> {:error, %{response | status_code: 400}}
     end
   end
 
   defp decode_body_from_response({:ok, body, _http_req}), do: Poison.decode(body)
   defp decode_body_from_response({:error, _}), do: {:error, 400}
+
+  defp convert_dashes(map) do
+    new_map = Enum.into(map, %{}, fn
+      {key, el} when is_binary(el) ->
+        key = String.replace(key, "-", "_")
+        {key, el}
+      {key, map} when is_map(map) ->
+        new_key = String.replace(key, "-", "_")
+        new_map = convert_dashes(map)
+        {new_key, new_map}
+      val -> val
+    end)
+  end
 end
--- spec/wok_rest_commons/request_parser_spec.exs
+++ spec/wok_rest_commons/request_parser_spec.exs
@@ -8,12 +8,40 @@ defmodule WokRESTCommons.RequestParserSpec do
 
     context "when body is correct json" do
       let :json_string, do: "{\"id\": 1, \"names\": [1,2,3]}"
-      it do: is_expected |> to(eq({:ok, %Response{decoded_body: %{"id" => 1, "names" => [1, 2, 3]}}}))
+      it do: is_expected.to eq({:ok, %Response{decoded_body: %{"id" => 1, "names" => [1, 2, 3]}}})
     end
 
     context "when malformed json" do
       let :json_string, do: "{id: 1,}"
-      it do: is_expected |> to(eq({:error, %Response{status_code: 400}}))
+      it do: is_expected.to eq({:error, %Response{status_code: 400}})
     end
+
+    context "by default it should convert dasherized-attributes to snake_case" do
+      let :json_string, do: "{\"id\": 1, \"code-version\": \"1\"}"
+      it do: is_expected.to eq({:ok, %Response{decoded_body: %{"id" => 1, "code_version" => "1"}}})
+    end
+
+    context "should convenrt nested maps as well" do
+      let :json_string do %{ "id" => 1,
+                            "code-version" => %{
+                              "nested-map-key" => "hello",
+                              "nested2" => %{
+                                "double-nested-key" => "key"
+                              }
+                            }
+                          } |> Poison.encode!
+      end
+
+      it do: is_expected.to eq({:ok, %Response{decoded_body: %{
+        "id" => 1,
+        "code_version" => %{
+          "nested_map_key" => "hello",
+          "nested2" => %{
+            "double_nested_key" => "key"
+          }
+        }}
+      }})
+    end
+
   end
 end
